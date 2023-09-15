9dd4efcbccc51d2b12be81ce8628d87c7e416d8e
Dominos0321 <dsch792@gmail.com>
2023-09-15 12:11:53 +0200

    end project

--- app/src/main/java/com/example/zadmobilki1509/MainActivity.kt
+++ app/src/main/java/com/example/zadmobilki1509/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.zadmobilki1509
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.provider.MediaStore
+import android.widget.Button
+import androidx.core.app.ActivityCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var btnPicture = findViewById<Button>(R.id.btn1)
+
+        btnPicture.isEnabled = false;
+
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 111)
+        }
+
+        else {
+            btnPicture.isEnabled = true;
+        }
+
+
+        btnPicture.setOnClickListener {
+            var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(i, 111)
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
 }
